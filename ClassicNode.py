57936403a5ea0ca34e60edcd37bbeaa0192f3dea
sam <sampoirier@potentialmotors.com>
2020-07-11 00:09:55 -0300

    Adding new code

--- ClassicNode.py
+++ ClassicNode.py
@@ -16,8 +16,8 @@ def callback_number(msg):
     dat = msg.data
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.flush()
+    # ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    # ser.flush()
 
     rospy.init_node('ClassicNode')
 
@@ -27,8 +27,9 @@ if __name__ == '__main__':
     # Now we start the real code for control of the vehicle
     while not rospy.is_shutdown() and not done:
         # Delete this when sending full user data is working
-        ser.write(str(dat).encode('utf-8'))
+        # ser.write(str(dat).encode('utf-8'))
         # msg = Int16MultiArray()
         # msg.data = pub_array
         # pub.publish(msg)
         # rate.sleep()
+        print(dat)
--- GUI.py
+++ GUI.py
@@ -5,11 +5,76 @@
 
 # import numpy as np
 import rospy
+import wx
 from std_msgs.msg import Int16, Int16MultiArray
 
 # Here you can put any of your initialization code
+global msg
 
 # AddGUI widget code here
+def onButton1(event):
+    print "1"
+    msg = 1
+    pub.publish(msg)
+    rate.sleep()
+def onButton2(event):
+    print "2"
+    msg = 2
+    pub.publish(msg)
+    rate.sleep()
+def onButton3(event):
+    print "3"
+    msg = 3
+    pub.publish(msg)
+    rate.sleep()
+def onButton4(event):
+    print "4"
+    msg = 4
+    pub.publish(msg)
+    rate.sleep()
+def onButton5(event):
+    print "5"
+    msg = 5
+    pub.publish(msg)
+    rate.sleep()
+def onButton6(event):
+    print "6"
+    msg = 6
+    pub.publish(msg)
+    rate.sleep()
+def onButton7(event):
+    print "7"
+    msg = 7
+    pub.publish(msg)
+    rate.sleep()
+
+# Set up window
+app = wx.App()
+frame = wx.Frame(None, -1, 'win.py')
+frame.SetDimensions(0,0,500,500)
+
+panel = wx.Panel(frame, wx.ID_ANY)
+
+#Buttons
+forward1 = wx.Button(panel, wx.ID_ANY, 'Forward', (105, 10))
+forward1.Bind(wx.EVT_BUTTON, onButton1)
+backward2 = wx.Button(panel, wx.ID_ANY, 'Backward', (105, 200))
+backward2.Bind(wx.EVT_BUTTON, onButton2)
+right3 = wx.Button(panel, wx.ID_ANY, 'Right', (200, 105))
+right3.Bind(wx.EVT_BUTTON, onButton3)
+left4 = wx.Button(panel, wx.ID_ANY, 'Left', (10, 105))
+left4.Bind(wx.EVT_BUTTON, onButton4)
+launch5 = wx.Button(panel, wx.ID_ANY, 'Launch', (105, 105))
+launch5.Bind(wx.EVT_BUTTON, onButton5)
+inc_speed6 = wx.Button(panel, wx.ID_ANY, '+ Speed', (300, 50))
+inc_speed6.Bind(wx.EVT_BUTTON, onButton6)
+dec_speed7 = wx.Button(panel, wx.ID_ANY, '- Speed', (300, 150))
+dec_speed7.Bind(wx.EVT_BUTTON, onButton7)
+
+#Show window
+# frame.Show()
+# frame.Centre()
+# app.MainLoop()
 
 if __name__ == '__main__':
 
@@ -21,8 +86,11 @@ if __name__ == '__main__':
     while not rospy.is_shutdown():
         # Any road that needs to be constantly running can go here. This will just stop the code if there is a
         # disconnect from the ROS network.
-
-        msg = input("Please enter a number 1-7 here:/n")
+        # Show window
+        frame.Show()
+        frame.Centre()
+        app.MainLoop()
+        # msg = input("Please enter a number 1-7 here:/n")
 
         # Publishing data to User Topic
         ## We can write whatever values we want to each of these variables and they will be send to the Raspi.
